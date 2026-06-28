db52ffb8d552a6e577c1ee42dfdd2b36d223cb18
sriharirv2007 <sriharirv2007@gmail.com>
2026-06-28 11:12:58 +0530

    Time: 0 ms (100%), Space: 12.2 MB (92.63%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def myPow(self, x, n):
+        a=x**n
+        return a
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        
