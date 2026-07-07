39c61abc43ec0c5df12c6793bc1b6dffa10bf744
sriharirv2007 <sriharirv2007@gmail.com>
2026-07-07 18:28:18 +0530

    Time: 0 ms (100%), Space: 12.3 MB (52.17%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def divisorGame(self, n):
+        if n%2==0:
+            return True
+        return False
+        
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
