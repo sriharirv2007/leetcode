898d6b8083dbefcf1fd51bab1bb10c696323b0c1
sriharirv2007 <sriharirv2007@gmail.com>
2026-07-15 22:14:27 +0530

    Time: 1168 ms (9.73%), Space: 12.2 MB (89.57%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def mySqrt(self, x):
+        
+        i = 0
+
+        while i * i <= x:
+            i += 1
+
+        return i-1
+
+
+       
+           
+                    
+       
+
+
+
+
+        
+
+        """
+        :type x: int
+        :rtype: int
+        """
+        
