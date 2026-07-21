6c13f6a30eb0ebe89e5895a978501d52bda1b5a4
sriharirv2007 <sriharirv2007@gmail.com>
2026-07-21 17:41:21 +0530

    Time: 0 ms (100%), Space: 13 MB (29.25%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -9,25 +9,4 @@ class Solution(object):
     
         
 
-    '''def lth(self,l,n,t):
-        if n[l-1]<=t:
-                if n[l-1]<t:
-                    return l
-                return l-1
-        else:
-            l=l//2
-            if l==0:
-                return 0
-            return self.lth(l,n,t)
-
-    def searchInsert(self, nums, target):
-        l=len(nums)
-        return self.lth(l,nums,target)'''
-        
-
-            
-           
-
-
     
-        
